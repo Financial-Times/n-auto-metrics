86b92b1070825723ebe402114c62db82374318ee
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-08-14 15:58:46 +0100

    minor cleanup  🐿 v2.8.0

--- src/action.js
+++ src/action.js
@@ -1,6 +1,27 @@
 import { createEnhancer, isPromise } from '@financial-times/n-express-enhancer';
 
-import { metricsEvent } from './event';
+import { getMetricsInstance } from './init';
+
+const createEventMetrics = ({ service, operation, action }) => {
+	const metrics = getMetricsInstance();
+	if (metrics === undefined) {
+		throw Error(`auto metrics instance needs to be initialised first`);
+	}
+	const serviceRoot = `service.${service}.action.${action}`;
+	const operationRoot = `operation.${operation}.action.${action}`;
+	const countOne = path => {
+		metrics.count(`${serviceRoot}.${path}`, 1);
+		metrics.count(`${operationRoot}.${path}`, 1);
+	};
+	return {
+		start: () => countOne('state.start'),
+		success: () => countOne('state.success'),
+		failure: e => {
+			countOne(`state.failure.category.${e.category}.status.${e.status}`);
+			countOne(`state.failure.category.${e.category}.type.${e.type}`);
+		},
+	};
+};
 
 const metricsAction = actionFunction => (paramsOrArgs = {}, meta) => {
 	const m = { ...meta, ...paramsOrArgs.meta };
@@ -13,7 +34,8 @@ const metricsAction = actionFunction => (paramsOrArgs = {}, meta) => {
 	}
 	const operation = m.operation || paramsOrArgs.operation;
 
-	const event = metricsEvent({ service, operation, action });
+	const event = createEventMetrics({ service, operation, action });
+	event.start();
 
 	try {
 		const call = meta
